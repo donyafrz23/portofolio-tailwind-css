2972f6f38015c5729fed32c5ece7d58841d6fc50
Dony Afrizal <afrz.dony@gmail.com>
2022-05-15 04:16:51 +0700

    Menambahkan foto dan deksripsi konten

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#06b6d4',
-        secondary: '#94a3b8'
+        primary: "#06b6d4",
+        secondary: "#94a3b8",
       },
       screens: {
-        '2xl': '1320px'
-      }
+        "2xl": "1320px",
+      },
     },
   },
   plugins: [],
-}
+};
