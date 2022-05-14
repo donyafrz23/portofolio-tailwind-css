7ec2e4771a45091d8da3263cadea58d328ddf12c
Dony Afrizal <afrz.dony@gmail.com>
2022-05-14 16:09:19 +0700

    Portofolio Dony Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#06b6d4',
+        secondary: '#94a3b8'
+      },
+      screens: {
+        '2xl': '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
